6a26931b079b9ae2d083a52d5d8e2d2a320f8ed4
Trung <trunghuynh@live.com>
2018-07-08 15:54:55 +1200

    add utility post, put, and correct base url

--- Scripts/shopfrontend/src/data/config.js
+++ Scripts/shopfrontend/src/data/config.js
@@ -1,1 +1,27 @@
-export const BASE_URL = ""
+export const BASE_URL = "/api/"
+
+export const fetchPost = (url, data) => fetch(url, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+});
+
+export const fetchPut = (url, data) => fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+});
+
+export const fetchDelete = (url) => fetch(url, {
+    method: 'DELETE',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+});
