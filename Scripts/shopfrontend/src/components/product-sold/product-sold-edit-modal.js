c55334e8f39c1a15e8bae860447e50985278585c
Trung <trunghuynh@live.com>
2018-07-10 22:58:30 +1200

    fix close form edit error on productsold

--- Scripts/shopfrontend/src/components/product-sold/product-sold-edit-modal.js
+++ Scripts/shopfrontend/src/components/product-sold/product-sold-edit-modal.js
@@ -1,5 +1,7 @@
-import { Modal, Button } from "react-bootstrap"
 import React, { Component } from "react"
+import { Modal, Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap"
+import DateTimePicker from 'react-datetime-picker';
+import "./clock.css"
 
 class ProductSoldEditModal extends Component {
 
@@ -8,35 +10,45 @@ class ProductSoldEditModal extends Component {
         this.handleShow = this.handleShow.bind(this);
         this.handleClose = this.handleClose.bind(this);
         this.handleChange = this.handleChange.bind(this);
-        
+        var date_string = props.productSold ? props.productSold.date_sold: new Date().toISOString();
         this.state = {
             show: props.show,
             store_id: props.productSold ? props.productSold.store_id : "",
             product_id: props.productSold ? props.productSold.product_id : "",
             customer_id: props.productSold ? props.productSold.customer_id: "",
-            date_sold: props.productSold ? props.productSold.date_sold: "",
+            date_sold: date_string,
             id: props.productSold ? props.productSold.id : "",
             products: [],
             customers: [],
-            stores: []
+            stores: [],
+            date: new Date(date_string)
         };
-
-        console.log(this.state);
-
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    onChange = date => {
+        try {
+            this.setState({ date, date_sold: date.toISOString() });
+            console.log(date, this.state.date_sold);
+        } catch (err) {
+
+        }
+        
+    }
+
     componentWillReceiveProps(nextProps) {
+        var date_string = nextProps.productSold && nextProps.productSold.date_sold  
+            ? nextProps.productSold.date_sold: new Date().toISOString();
         if(nextProps.productSold) {
             this.setState({
                 id: nextProps.productSold.id,
-                date_sold: nextProps.productSold.date_sold,
+                date_sold: date_string,
                 product_id: nextProps.productSold.product_id,
                 customer_id: nextProps.productSold.customer_id,
-                store_id: nextProps.productSold.store_id
+                store_id: nextProps.productSold.store_id,
+                date: new Date(date_string)
             });
         }
-
         if(nextProps.products) {
             this.setState({
                 products: nextProps.products,
@@ -44,7 +56,6 @@ class ProductSoldEditModal extends Component {
                 stores: nextProps.stores
             });
         }
-        
     }
 
     handleSubmit(e) {
@@ -75,23 +86,25 @@ class ProductSoldEditModal extends Component {
             
         });
         return (
-            <select name={name}>{options}</select>
+            <FormControl componentClass="select" name={name} placeholder="select">
+                {options}
+            </FormControl>
         );
     }
 
     initEmptyProductSold() {
         if(! this.state.date_sold) {
-            this.state.date_sold = new Date().toISOString();
+            this.setState({date_sold: new Date().toISOString(), date: new Date()});
         }
         try {
             if(! this.state.customer_id) {
-                this.state.customer_id = this.state.customers[0].id;
+                this.setState({customer_id: this.state.customers[0].id});
             }
             if(! this.state.product_id) {
-                this.state.product_id = this.state.products[0].id;
+                this.setState({product_id: this.state.products[0].id});
             }
             if(! this.state.store_id) {
-                this.state.store_id = this.state.stores[0].id;
+                this.setState({store_id: this.state.stores[0].id});
             }
         } catch (error) {
             
@@ -111,11 +124,26 @@ class ProductSoldEditModal extends Component {
                 </Modal.Header>
                 <Modal.Body>
                     <form onSubmit={this.handleSubmit} onChange={this.handleChange}>
-                        <p>Stores: {this.renderSelect(this.state.stores, this.state.store_id, 'store_id')}</p>
-                        <p>customers: {this.renderSelect(this.state.customers, this.state.customer_id, 'customer_id')}</p>
-                        <p>Products: {this.renderSelect(this.state.products, this.state.product_id, 'product_id')}</p>
+                        <FormGroup controlId="formControlsStore">
+                            <ControlLabel>Store:</ControlLabel>
+                            {this.renderSelect(this.state.stores, this.state.store_id, 'store_id')}
+                        </FormGroup>
+                        <FormGroup controlId="formControlsCustomer">
+                            <ControlLabel>Customer:</ControlLabel>
+                            {this.renderSelect(this.state.customers, this.state.customer_id, 'customer_id')}
+                        </FormGroup>
+                        <FormGroup controlId="formControlsProduct">
+                            <ControlLabel>Product:</ControlLabel>
+                            {this.renderSelect(this.state.products, this.state.product_id, 'product_id')}
+                        </FormGroup>
+                        <FormGroup controlId="formControlsDate">
+                            <ControlLabel>Date sold (Month/date/year Hour: minute):</ControlLabel><br />
+                            <DateTimePicker clockClassName="clock" calendarClassName="clock"
+                                onChange={this.onChange}
+                                value={this.state.date}
+                                />
+                        </FormGroup>
                         <input type="hidden" name="id" value={this.state.id}/>
-                        <p>Date sold: <input name="date_sold" value={this.state.date_sold} /></p>
                         <p><input type="submit" value="Submit" /></p>
                     </form>
                 </Modal.Body>
